1ff8b20d2d6b42b43555d5ea10aa7e525890e9af
LeninMC <95388478+LeninMC@users.noreply.github.com>
2022-02-16 19:04:32 -0500

    commit : 19:00

---
+++ app/src/main/java/com/example/leninmopositacresposupletorio/MainActivityMCLB.java
@@ -0,0 +1,14 @@
+package com.example.leninmopositacresposupletorio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityMCLB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
